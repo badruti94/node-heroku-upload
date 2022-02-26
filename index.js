c525328ee53c291ae4bbaee8783c3a26a15de432
Badru <badruakfm@gmail.com>
2022-02-26 17:25:39 +0700

    update

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ const cors = require('cors')
 const app = express()
 
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${new Date().getTime()}-${file.originalname}`)
-    }
-})
+const storage = multer.memoryStorage()
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
